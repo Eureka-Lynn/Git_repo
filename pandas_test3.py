620e625562bf565fdecc0bc360ca4b849b331fab
luyee_lynn <eureka_lynn@163.com>
2024-03-04 15:20:34 +0800

    null

--- pandas_test3.py
+++ pandas_test3.py
@@ -6,8 +6,8 @@ df=pd.read_csv(fpath)
 df.loc[:,'bWendu']=df['bWendu'].str.replace('℃','').astype('int32')
 df.loc[:,'yWendu']=df['yWendu'].str.replace('℃','').astype('int32')
 
-df.assign(
-    yWendu_huashi = lambda x : x["yWendu"] * 9 / 5 + 32,
-    # 摄氏度转华氏度
-    bWendu_huashi = lambda x : x["bWendu"] * 9 / 5 + 32
-)
+df['wencha_type']=''
+df.loc[df['bWendu']-df['yWendu']>10,'wencha_type']='温差大'
+df.loc[df['bWendu']-df['yWendu']<=10,'wencha_type']='温差小'
+
+print(df['wencha_type'].value_counts())
