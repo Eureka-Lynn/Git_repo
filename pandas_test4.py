3e602bd5ac041acd6c3c0cee3bf4f3863035f2a9
luyee_lynn <eureka_lynn@163.com>
2024-03-06 12:03:54 +0800

    pandas

--- pandas_test4.py
+++ pandas_test4.py
@@ -5,12 +5,12 @@ fpath=('Tai1.csv')
 
 df=(pd.read_csv(fpath))
 
-df.set_index('时间',inplace=True)
 
 # 时长转换为分钟
 df.loc[:,'time']=df['时长']/60
-df[u'time']=df[u'time'].apply(lambda x : format (x,'.2'))
-df['time']=df['time'].astype('float32')
 
-
+print(df.describe())
+print('==========================================')
+print(df[df['time']>=30])
+print('==========================================')
 print(df.loc[df['time']>=30,:])
