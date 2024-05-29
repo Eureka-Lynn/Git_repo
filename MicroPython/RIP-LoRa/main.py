156226f72452f4617eb05e7c2979af497e497431
luyee_lynn <eureka_lynn@163.com>
2024-05-29 21:38:51 +0800

    null

--- MicroPython/RIP-LoRa/main.py
+++ MicroPython/RIP-LoRa/main.py
@@ -1,4 +1,14 @@
 from machine import Pin,UART
+import math
 import time
 
 lora = UART(9600,tx=0,rx=1)
+radar = UART(115200,tx=6,rx=7)
+
+def get_radar_data():
+    data = radar.read()
+    if data:
+        target = data[7:-4]
+        
+
+def detect_person():
