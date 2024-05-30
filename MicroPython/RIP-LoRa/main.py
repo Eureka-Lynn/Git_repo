995846e58d416509fd8c65982af7e459786f5464
luyee_lynn <eureka_lynn@163.com>
2024-05-30 23:04:40 +0800

    null

--- MicroPython/RIP-LoRa/main.py
+++ MicroPython/RIP-LoRa/main.py
@@ -4,8 +4,9 @@ import time
 
 lora = UART(9600,tx=0,rx=1)
 radar = UART(115200,tx=6,rx=7)
-led = PWM(Pin(_,Pin.OUT))
+led = PWM(Pin(25,Pin.OUT))
 current_brightness = 0
+led.freq(1000)
 
 # 获取雷达数据
 def get_radar_data():
@@ -35,8 +36,10 @@ def get_node_id():
     return int(time.time() * 1000) % 10000
 
 # 广播信息
-def send_message(node_id,time_stamp):
-    message = '{0}:{1}:'.format(node_id,time_stamp)
+def send_message(node_id,current_brightness):
+    time_stamp = time.time()
+    message = '{0}:{1}:{2}'.format(node_id,time_stamp,current_brightness)
+    lora.write(message)
 
 # 接收信息
 def receive_message():
@@ -50,3 +53,20 @@ def change_brightness(LED_Duty):
     while LED_Duty > current_brightness:
         current_brightness += 1
         led.duty_u16(current_brightness)
+    while LED_Duty < current_brightness:
+        current_brightness -= 1
+        led.duty_u16(current_brightness)
+
+# 处理LoRa信息
+def handle_LoRa(message):
+    Lora_Data = message.split(':')
+    Node_id = Lora_Data[0]
+    Time_Stamp = Lora_Data[1]
+    Node_Brightness = Lora_Data[2]
+    return Node_id,Time_Stamp,Node_Brightness
+
+# 相邻节点发现
+def neighbor_find():
+    current_time = time.time()
+    message = receive_message()
+    handle_LoRa(message)
